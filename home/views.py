ece58a1d38c582820fd9e61f0f51d1136d813964
Onkar-Bynry <onkar.pathak@bynry.io>
2024-10-07 23:49:07 +0530

    Remove files that should be ignored

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def index(request):
     courses = {
         'course_name': 'python',
